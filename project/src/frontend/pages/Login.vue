5f378da3eb2d2db7ab68b90b11291b0cafb8e5b4
fabiocarvalho1998 <epicshootgamer@gmail.com>
2020-11-18 19:24:33 +0000

    updates  + ListZones Routed

--- project/src/frontend/pages/Login.vue
+++ project/src/frontend/pages/Login.vue
@@ -1,38 +1,50 @@
 <template>
  <div>
-   <form class="login" @submit.prevent="login">
+   <form class="login">
      <h1>Sign in</h1>
      <label>User name</label>
      <input required v-model="username" type="text" placeholder="Username"/>
+     <h1>{{username}}</h1>
      <hr/>
      <label>Password</label>
      <input required v-model="password" type="password" placeholder="Password"/>
+     <h1>{{password}}</h1>
      <hr/>
-     <button type="submit">Login</button>
+     <button v-on:click.prevent="handlerSubmit">Login</button>
    </form>
  </div>
 </template>
 
 <script>
 
-import { AUTH_REQUEST } from '../store/actions/auth'
-
 export default {
   name: 'Login',
-  data() {
+  data(){
     return {
       username: this.username,
       password: this.password
     };
   },
-  methods: {
-    login: function() {
-      const { username, password } = this;
-      this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
-        //this.$router.push("/");
-        print(this.username)
-        print(this.password)
-      });
+ methods: {
+    handlerSubmit: function(){
+      this.$http.post('http://localhost:8081/login', {  //não aceita endereço docker pois é no browser. Como porta é diferente origina problema de cors
+          username: this.username,
+          password: this.password
+        }).then(response => {
+          let is_admin = response.data.user.is_admin
+          localStorage.setItem('user',JSON.stringify(response.data.user))
+          localStorage.setItem('jwt',response.data.token)
+
+          if (localStorage.getItem('jwt') != null){
+            if(is_admin== 1){
+              this.$router.push('admin')
+            }else {
+              this.$router.push('map')
+            }
+          }
+        }).catch(function (error) {
+          console.error(error.response);
+        });
     }
   }
 }
--- project/src/main.js
+++ project/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueGeolocation from 'vue-browser-geolocation'
 import * as VueGoogleMaps from 'vue2-google-maps'
-
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Login from './frontend/pages/Login.vue'
+import Map from './frontend/pages/Map.vue'
+import ListZones from './frontend/pages/ListZones.vue'
 
 import App from './App.vue'
 
@@ -12,6 +16,8 @@ import App from './App.vue'
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 Vue.use(VueGeolocation)
+Vue.use(VueRouter)
+Vue.use(VueResource)
 Vue.use(VueGoogleMaps,  {
   load: {
     key: 'AIzaSyDvzBG1YC-EqqOau_4BMMAgK7p-t9nrYjE',
@@ -20,6 +26,27 @@ Vue.use(VueGoogleMaps,  {
   installComponents: true,
 })
 
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: Map
+    },
+    {
+      path: '/zones',
+      name: 'zones',
+      component: ListZones
+    }
+  ]
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
